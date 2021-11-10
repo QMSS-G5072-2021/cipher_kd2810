f8139f5221f92824466d3718b941e0bee4f86c05
Kevin Davis <kp.davis@outlook.com>
2021-11-10 14:15:32 -0500

    test: add unit test for cipher

--- tests/test_cipher_kd2810.py
+++ tests/test_cipher_kd2810.py
@@ -1,1 +1,16 @@
 from cipher_kd2810 import cipher_kd2810
+from cipher_kd2810.cipher_kd2810 import cipher
+
+import pytest
+
+@pytest.mark.parametrize("example_str, example_shift, expected",[
+    ("KevIn", 2, "MgxKp"),
+    ("PerSAuD", 4, "TivWEyH"),
+    ("macHInes", -5, "hVXCDiZn"),
+    ("eagle", 9, "njpun"),
+    ("EAGLE", -10, "uqwBu"),
+    ("DaTa Sci", 2, "FcVc Uek")
+])
+def test_several_ciphers(example_str, example_shift, expected):
+    result = cipher(example_str, example_shift)
+    assert (result == expected)
